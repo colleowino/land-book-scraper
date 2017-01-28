bd264f3994408a81f521be8f552796702e033eb8
colleowino <colleowino@gmail.com>
2017-01-28 03:48:12 +0300

    added code to get title, image and homepage link

--- index.js
+++ index.js
@@ -2,26 +2,32 @@ var request = require('request');
 var cheerio = require('cheerio');
 var $q = require('q');
 
-var url = 'https://land-book.com/websites/179';
-
+// should start at 179
 function loadPage(id){
-
 	return new Promise(function(resolve, reject){
 		var url = 'https://land-book.com/websites/'+id;
 		request(url,function(err,resp,body){
 			if(!err)
 				console.log("loaded: "+id);
-				resolve(50);
+				resolve(body);
 		});
 
 	});
 }
 
+function parseBody(body){
+	var $ = cheerio.load(body);
+	//var title = $('h2').text();
+	//var homepage = $('.icon-world + a').text();
+	var img = $('#website-screenshot').attr('src');
+	console.log(img);
+}
+
 var promises = [];
 
-for(var i=10; i < 15; i++){
-	promises.push(loadPage(i));
-}
+//for(var i=10; i < 15; i++){
+	//promises.push(loadPage(i));
+//}
 
 //$q.all(promises).then(function(response){
 	//console.log(response);
@@ -29,4 +35,12 @@ for(var i=10; i < 15; i++){
 //});
 
 
+loadPage(2080).then(function(body){
+	try{
+		parseBody(body);
+	}
+	catch(err){
+		console.log(err);
+	}
+});
 
