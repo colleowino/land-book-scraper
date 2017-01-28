190889eb044eb7bd1e40bff52c5eb9c0c35a4dcf
colleowino <colleowino@gmail.com>
2017-01-28 04:46:38 +0300

    added ability to write to save image

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var $q = require('q');
+var fs = require('fs');
+var path = require('path');
 
 // should start at 179
 function loadPage(id){
@@ -23,7 +25,41 @@ function parseBody(body){
 	console.log(img);
 }
 
-var promises = [];
+function getExtName(url){
+	return path.extname(url).split('?')[0];
+}
+
+// landbook images seem to lack 'http:' prefix
+function fixUrlPrefix(url){
+	console.log(url);
+	if(!url.includes('http:')){
+		url = 'http:'+url;
+	}
+	return url;
+}
+
+function downloadImage(url,fileId){
+	var downloadDir = 'downloads';
+
+	if(!fs.existsSync(downloadDir))
+		fs.mkdirSync(downloadDir);
+
+	return new Promise(function(resolve,reject){
+		var filename = path.join(downloadDir,fileId.toString() + getExtName(url));
+		
+		request(fixUrlPrefix(url)).pipe(fs.createWriteStream(filename)).on('close',function(){
+			resolve(fileId);
+		});
+
+	});
+
+}
+
+var imgLink = 'http://s3-eu-central-1.amazonaws.com/land-book-production/websites/screenshots/000/001/800/large/a084c455-2baa-403a-b74d-38a0477aa6f8.jpg?1476078292'
+
+downloadImage(imgLink,1).then(function(result,reject){
+	console.log('downloaded '+result);
+});
 
 //for(var i=10; i < 15; i++){
 	//promises.push(loadPage(i));
@@ -35,12 +71,12 @@ var promises = [];
 //});
 
 
-loadPage(2080).then(function(body){
-	try{
-		parseBody(body);
-	}
-	catch(err){
-		console.log(err);
-	}
-});
+//loadPage(2080).then(function(body){
+	//try{
+		//parseBody(body);
+	//}
+	//catch(err){
+		//console.log(err);
+	//}
+//});
 
